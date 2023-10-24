1ead433f957eed065ca188220df6e53344658190
John Cole <john.cole@liberty-it.co.uk>
2023-10-24 10:09:22 +0100

    forms part 1

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,29 @@
 import './Search.css';
+import {useState} from "react";
 
 const Search = () => {
 
+    const [orderId, setOrderId] = useState("");
+
+    const handleChange = (event) => {
+        console.log(event.target)
+        console.log(event.target.value);
+        console.log(event.target.id);
+        setOrderId(event.target.value);
+    }
+
+    const doSearch = (event) => {
+        event.preventDefault();
+        console.log("searching for " + orderId);
+    }
+
     return (
         <div className="searchBox">
-            <label htmlFor="orderId">Order Id:</label>
-            <input id="orderId" type="text" />
-            <button>Search</button>
+            <form onSubmit={doSearch}>
+                <label htmlFor="orderId">Order Id:</label>
+                <input onChange={handleChange} value={orderId} id="orderId" type="text" />
+                <button type="submit">Search</button>
+            </form>
         </div>
     );
 }
